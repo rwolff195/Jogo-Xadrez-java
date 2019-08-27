6677b54c39886d3c144f14b1f4180c897d52a5f6
Rafael Wolff de Lima <rafael.wolff@linx.com.br>
2019-08-26 23:22:48 -0300

    Primeira classe

--- src/aplicacao/ProgPrincipal.java
+++ src/aplicacao/ProgPrincipal.java
@@ -1,9 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class ProgPrincipal {
 
 	public static void main(String[] args) {
-		System.out.println("Ola mundo!!!!");
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
